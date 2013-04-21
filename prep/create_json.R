cb7783111a97e8685b240788445818eed59a41d0
patrick <chalkypirate@gmail.com>
2013-04-20 23:13:26 -0700

    Added prep scripts/web files.

---
+++ prep/create_json.R
@@ -0,0 +1,20 @@
+
+cities <- read.csv('cities.csv', as.is=TRUE)
+cities <- cities[!is.na(cities$Latitude),]
+n.cities <- nrow(cities)
+city.list <- list()
+for (i in 1:n.cities) {
+  city <- cities$City[i]
+  country <- cities$Country[i]
+  latitude <- cities$Latitude[i]
+  longitude <- cities$Longitude[i]
+  zoom <- cities$Zoom[i]
+  lst <- list(city=city, country=country,
+              latitude=latitude, longitude=longitude, zoom=zoom)
+  city.list[[i]] <- lst
+}
+city.list <- list(cities=city.list)
+city.json <- toJSON(city.list)
+sink("../web/cities.json")
+cat(city.json)
+sink()
---
+++ prep/geocode_cities.R
@@ -0,0 +1,44 @@
+
+library(rjson)
+library(XML)
+
+geocode <- function(place) {
+  require(XML)
+  address <- gsub("[[:blank:]+]", "+", place)
+  url <- "http://maps.googleapis.com/maps/api/geocode/xml?address="
+  url <- paste(url, place, "&sensor=true", sep="")
+  response <- xmlTreeParse(url, useInternal=TRUE)
+  status <- xmlValue(getNodeSet(response, "//status")[[1]])
+  if (status == "OK") {
+    lat <- as.numeric(xmlValue(getNodeSet(response, '///lat')[[1]]))
+    lng <- as.numeric(xmlValue(getNodeSet(response, '///lng')[[1]]))
+  } else {
+    lat <- lng <- NA
+  }
+  return(list(lat=lat, lng=lng))
+}
+
+## Load current version of cities table
+cities <- read.csv('cities.csv', as.is=TRUE)
+
+## Geocode any new additions
+## (i.e. cities without a set lat/lng/zoom)
+n.cities <- nrow(cities)
+for (i in 1:n.cities) {
+  if (is.na(cities$Latitude[i])) {
+    city <- cities$City[i]
+    print(city)
+    country <- cities$Country[i]
+    coords <- geocode(paste(city, ", ", country, sep=""))
+    print(coords)
+    cities$Latitude[i] <- coords[["lat"]]
+    cities$Longitude[i] <- coords[["lng"]]
+    Sys.sleep(5)
+  }
+}
+
+## Save
+write.csv(cities, 'cities.csv', row.names=FALSE)
+
+
+
