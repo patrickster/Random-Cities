5f669bc375989cbd0f7c4995ab5990c165d412be
patrick <chalkypirate@gmail.com>
2013-04-22 23:12:22 -0700

    Cleaned up scripts.

--- prep/create_json.R
+++ prep/create_json.R
@@ -1,8 +1,11 @@
 
+library(rjson)
+
 cities <- read.csv('cities.csv', as.is=TRUE)
 cities <- cities[!is.na(cities$Latitude),]
 n.cities <- nrow(cities)
 city.list <- list()
+
 for (i in 1:n.cities) {
   city <- cities$City[i]
   country <- cities$Country[i]
@@ -13,6 +16,7 @@ for (i in 1:n.cities) {
               latitude=latitude, longitude=longitude, zoom=zoom)
   city.list[[i]] <- lst
 }
+
 city.list <- list(cities=city.list)
 city.json <- toJSON(city.list)
 sink("../web/cities.json")
